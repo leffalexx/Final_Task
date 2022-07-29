bbc6788d9597a3f0cb13dc806ce8b2c80617001e
aleksei_namestnikov <leffalexx@gmail.com>
2022-07-29 11:34:37 +0300

    announced the array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"Car", "wheel", "Fox", "Parliament", "Tree", "322", "sea", "--09"};
