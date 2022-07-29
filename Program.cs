5801d1d7f88af298c54a52a7afa8aec8f11769ef
aleksei_namestnikov <leffalexx@gmail.com>
2022-07-29 11:40:31 +0300

    Determined array size in the main method

--- Program.cs
+++ Program.cs
@@ -6,5 +6,21 @@ void ShowArray(string[] array)
       Console.Write(array[i] + " ");
    Console.WriteLine();
 }
+
+string[] ThreeDigitArray(string[] array)
+{
+   int resultArraySize = 0;
+   int resultArrayIndex = 0;
+
+   for (int i = 0; i < array.Length; i++) 
+   {
+      if (array[i].Length <= 3)
+      {
+         resultArraySize++;
+      }
+   }
+
+}
+
 ShowArray(array);
 Console.WriteLine();
