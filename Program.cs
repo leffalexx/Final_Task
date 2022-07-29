31965e9d95979965c32814d37492b65b0b004b52
aleksei_namestnikov <leffalexx@gmail.com>
2022-07-29 11:37:43 +0300

    made ShowArray method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
 ﻿string[] array = {"Car", "wheel", "Fox", "Parliament", "Tree", "322", "sea", "--09"};
+
+void ShowArray(string[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+      Console.Write(array[i] + " ");
+   Console.WriteLine();
+}
+ShowArray(array);
+Console.WriteLine();
